277781d2a9436bb90bb4a8c10c4bed32e55dc513
amareetkm2024-del <amareetk.m.2024@smu.edu.sg>
2025-10-10 18:37:08 +0800

    Update mock_pets.js

--- mock_pets.js
+++ mock_pets.js
@@ -10,13 +10,13 @@ const pets = [
     gender: "Female",
     temperament: "Friendly, Gentle, Loyal",
     maintenanceLevel: "Medium",
-    image: "",      // missing image here to use api to replace pictire
+    image: "",      // missing image here to use api to replace picture
     adoptionAgency: "Happy Tails Shelter",
     description: "A sweet and affectionate retriever that loves playing fetch and being around people."
   },
   {
     id: 2,
-    name: "Milo",
+    name: "Fluffy",
     species: "Cat",
     breed: "British Shorthair",
     age: 2,
@@ -27,8 +27,23 @@ const pets = [
     adoptionAgency: "Purrfect Companions",
     description: "A calm and low-maintenance cat who enjoys lounging and occasional cuddles."
   },
-  {
+
+    {
     id: 3,
+    name: "Miko",
+    species: "Hamster",
+    breed: "Syrian Hamster",
+    age: 1,
+    gender: "Female",
+    temperament: "Energetic, Curious, Friendly",
+    maintenanceLevel: "Medium",
+    image: "https://images.unsplash.com/photo-1589130674510-6dadfc8a1266?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    adoptionAgency: "Rabbit Shelter",
+    description: "A mischevious and energetic hamster that loves to run."
+  }, 
+
+  {
+    id: 4,
     name: "Coco",
     species: "Dog",
     breed: "Poodle (Toy)",
@@ -36,12 +51,12 @@ const pets = [
     gender: "Female",
     temperament: "Intelligent, Energetic, Playful",
     maintenanceLevel: "High",
-    image: "",          // missing image here to use api to replace pictire
+    image: "",          // missing image here to use api to replace picture
     adoptionAgency: "Pet Haven",
     description: "An active and smart poodle who enjoys walks and mental challenges."
   },
   {
-    id: 4,
+    id: 5,
     name: "Luna",
     species: "Cat",
     breed: "Siamese",
@@ -55,7 +70,7 @@ const pets = [
   },
 
   {
-    id: 5,
+    id: 6,
     name: "Rocky",
     species: "Dog",
     breed: "Bulldog",
@@ -68,7 +83,7 @@ const pets = [
     description: "A calm and steady companion who enjoys short walks and naps."
   },
   {
-    id: 6,
+    id: 7,
     name: "Nala",
     species: "Cat",
     breed: "Maine Coon",
@@ -81,7 +96,7 @@ const pets = [
     description: "A fluffy Maine Coon who loves being brushed and playing with toys."
   },
   {
-    id: 7,
+    id: 8,
     name: "Buddy",
     species: "Dog",
     breed: "Beagle",
@@ -89,40 +104,25 @@ const pets = [
     gender: "Male",
     temperament: "Energetic, Curious, Friendly",
     maintenanceLevel: "Medium",
-    image: "",   // missing image here to use api to replace pictire
+    image: "",   // missing image here to use api to replace picture
     adoptionAgency: "City Paws Shelter",
     description: "A cheerful and curious Beagle who loves walks and exploring scents."
   },
 
-
   {
-    id: 8,
+    id: 9,
     name: "Bunni",
-    species: "Rabbit",
+    species: "Cat",
     breed: "Californian",
     age: 1,
     gender: "Female",
-    temperament: "Energetic, Curious, Friendly",
-    maintenanceLevel: "Medium",
+    temperament: "Affectionate, Playful, Gentle",
+    maintenanceLevel: "High",
     image: "https://images.unsplash.com/photo-1535241749838-299277b6305f?q=80&w=746&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    adoptionAgency: "Rabbit Shelter",
-    description: "A cheerful and curious Bunny who loves to eat."
-  },
+    adoptionAgency: "Rabbit Home",
+    description: "A fluffy Rabbit who loves to eat and munch on carrots while playing."
+  } 
 
-
-  {
-    id: 9,
-    name: "Miko",
-    species: "Hamster",
-    breed: "Syrian Hamster",
-    age: 1,
-    gender: "Female",
-    temperament: "Energetic, Curious, Friendly",
-    maintenanceLevel: "Medium",
-    image: "https://images.unsplash.com/photo-1589130674510-6dadfc8a1266?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    adoptionAgency: "Rabbit Shelter",
-    description: "A mischevious and energetic hamster that lvoes to run."
-  }
 
 ];
 
